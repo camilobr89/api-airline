47d9034f34b9a6ebde76a3fc2634e131a58b6abd
camilobr89 <camilobr89@hotmail.com>
2023-03-26 19:22:11 -0500

    modificacion final api

--- api/src/index.js
+++ api/src/index.js
@@ -2,17 +2,24 @@ import express from 'express';
 import flightRoutes from './routes/flight.routes.js';
 import cors from 'cors';
 
+
+
 // Inicializaciones
 const app = express();
 
 // Configuraciones
 app.use(express.json());
 
-// Middlewares
 app.use(cors());
 
 // Rutas
 app.use(flightRoutes);
+
+// Middlewares
+
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Not found' });
+});
 
 
 // Inicialización del servidor
